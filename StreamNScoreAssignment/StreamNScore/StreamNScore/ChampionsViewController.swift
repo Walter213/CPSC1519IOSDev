67db7b77937d4ae24837a89038290747098edd0c
Walter213 <46505629+Walter213@users.noreply.github.com>
2020-02-04 16:58:38 -0700

    Working on Assignment 2

---
+++ StreamNScoreAssignment/StreamNScore/StreamNScore/ChampionsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChampionsViewController.swift
+//  StreamNScore
+//
+//  Created by Walter Teljega on 2/4/20.
+//  Copyright © 2020 Walter Teljega. All rights reserved.
+//
+
+import UIKit
+
+class ChampionsViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
---
+++ StreamNScoreAssignment/StreamNScore/StreamNScore/EuropaViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EuropaViewController.swift
+//  StreamNScore
+//
+//  Created by Walter Teljega on 2/4/20.
+//  Copyright © 2020 Walter Teljega. All rights reserved.
+//
+
+import UIKit
+
+class EuropaViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
