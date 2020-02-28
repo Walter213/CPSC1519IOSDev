beb758a25a0e6280219d0e2f9e46da542df660e8
Walter213 <46505629+Walter213@users.noreply.github.com>
2020-02-28 16:03:32 -0700

    got controller set up know need to make table array

---
+++ StreamNScoreAssignment/StreamNScore/StreamNScore/SettingsController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsController.swift
+//  StreamNScore
+//
+//  Created by Walter Teljega on 2/28/20.
+//  Copyright © 2020 Walter Teljega. All rights reserved.
+//
+
+import UIKit
+
+class SettingsController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+
+}
