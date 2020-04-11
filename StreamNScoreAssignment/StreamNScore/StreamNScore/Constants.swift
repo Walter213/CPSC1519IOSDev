b274750f3bff45f143b13a7f7a9494a8a02a9747
Walter213 <46505629+Walter213@users.noreply.github.com>
2020-04-10 18:57:00 -0600

    Got settings done and set up, working on light and dark theme

---
+++ StreamNScoreAssignment/StreamNScore/StreamNScore/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  StreamNScore
+//
+//  Created by Walter Teljega on 2020-04-10.
+//  Copyright © 2020 Walter Teljega. All rights reserved.
+//
+
+import Foundation
+
+let usernameKey = "username";
+let passwordKey = "password";
+let roleKey = "role";
+let notificationKey = "notifications"
+
+let premierKey = "premier";
+let laligaKey = "laLiga";
+let seriaAKey = "seriaA";
+let bundesligaKey = "bundesliga";
+let ligue1Key = "ligue1";
--- StreamNScoreAssignment/StreamNScore/StreamNScore/SettingsController.swift
+++ StreamNScoreAssignment/StreamNScore/StreamNScore/SettingsController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class SettingsController: UIViewController
 {
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var roleLabel: UILabel!
+    @IBOutlet weak var notificationsLabel: UILabel!
+    
+    @IBOutlet weak var premierLabel: UILabel!
+    @IBOutlet weak var laLigaLabel: UILabel!
+    @IBOutlet weak var seriaALabel: UILabel!
+    @IBOutlet weak var bundesligaLabel: UILabel!
+    @IBOutlet weak var ligue1Label: UILabel!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -24,5 +35,51 @@ class SettingsController: UIViewController
     {
         return UIInterfaceOrientationMask(rawValue:
             (UIInterfaceOrientationMask.portrait.rawValue))
+    }
+    
+    @IBAction func goIntoSettings(_ sender: Any)
+    {
+        let application = UIApplication.shared
+        let url = URL(string: UIApplication.openSettingsURLString)! as URL
+        if application.canOpenURL(url) {
+            application.open(url, options:[UIApplication.OpenExternalURLOptionsKey(rawValue: ""):""] , completionHandler: nil)
+        }
+    }
+    
+    @objc func applicationWillEnterForeground(notification:NSNotification)
+    {
+        let defaults = UserDefaults.standard
+        defaults.synchronize()
+        refreshFields()
+    }
+    
+    func refreshFields()
+    {
+        let defaults = UserDefaults.standard
+        usernameLabel.text = defaults.string(forKey: usernameKey)
+        passwordLabel.text = defaults.string(forKey: passwordKey)
+        roleLabel.text = defaults.string(forKey: roleKey)
+        notificationsLabel.text = defaults.bool(forKey: notificationKey)
+            ? "Enabled" : "Disabled"
+        
+        premierLabel.text = defaults.string(forKey: premierKey)
+        laLigaLabel.text = defaults.string(forKey: laligaKey)
+        seriaALabel.text = defaults.string(forKey: seriaAKey)
+        bundesligaLabel.text = defaults.string(forKey: bundesligaKey)
+        ligue1Label.text = defaults.string(forKey: ligue1Key)
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        refreshFields()
+        let app = UIApplication.shared
+        NotificationCenter.default.addObserver(self, selector: #selector(self.applicationWillEnterForeground(notification:)), name: UIApplication.willEnterForegroundNotification, object: app)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool)
+    {
+        super.viewDidDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
     }
 }
